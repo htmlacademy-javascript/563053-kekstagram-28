28e29e024c7048b06aaccb11ffca37bfad17b1a3
Che5ter <hatake18@bk.ru>
2023-04-04 11:37:35 +0300

    homework module9 fixes 2

--- js/scale.js
+++ js/scale.js
@@ -4,7 +4,6 @@ const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
 
 const scaleLowButton = document.querySelector('.scale__control--smaller');
-const scaleUpButton = document.querySelector('.scale__control--bigger');
 const inputScale = document.querySelector('.scale__control--value');
 const imageElement = document.querySelector('.img-upload__preview img');
 const scaleContainer = document.querySelector('.img-upload__scale');
@@ -16,20 +15,16 @@ const scaleImage = (value) => {
 };
 
 const onPressScaleButtons = (evt) => {
+  const targetButton = evt.target === scaleLowButton;
   const currentValue = parseInt(inputScale.value, 10);
-  if (evt.target === scaleLowButton) {
-    let newValue = currentValue - SCALE_STEP;
-    if (newValue < MIN_SCALE) {
-      newValue = MIN_SCALE;
-    }
-    scaleImage(newValue);
-  } else if (evt.target === scaleUpButton) {
-    let newValue = currentValue + SCALE_STEP;
-    if (newValue > MAX_SCALE) {
-      newValue = MAX_SCALE;
-    }
-    scaleImage(newValue);
+  let newValue = targetButton ? currentValue - SCALE_STEP : currentValue + SCALE_STEP;
+  if (newValue < MIN_SCALE) {
+    newValue = MIN_SCALE;
   }
+  if (newValue > MAX_SCALE) {
+    newValue = MAX_SCALE;
+  }
+  scaleImage(newValue);
 };
 
 const resetScale = () => scaleImage(DEFAULT_SCALE);
